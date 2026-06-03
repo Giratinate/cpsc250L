b18f93dd2f6200fa9750f5dffb529838880811fe
Giratinate <caltho1@yahoo.com>
2026-06-03 13:12:50 -0400

    Allowed program to read a file.

--- labs/lab03_functions_modular_design/starter_code/temperature_report.py
+++ labs/lab03_functions_modular_design/starter_code/temperature_report.py
@@ -1,8 +1,14 @@
 def read_temperatures(filename):
-    pass
+    temperatures = []
+    with open(filename, "r") as f:
+        for line in f:
+            if line != "\n":
+                number = float(line.strip())
+                temperatures.append(number)
+    return temperatures
 
 def calculate_average(values):
-    pass
+    return sum(values) / len(values)
 
 def find_maximum(values):
     pass
@@ -14,6 +20,8 @@ def count_above_threshold(values, threshold):
     pass
 
 def print_report(values):
+    print("Temperature Report")
+    print(f"Average: {calculate_average(values):.2f}")
     pass
 
 def main():
