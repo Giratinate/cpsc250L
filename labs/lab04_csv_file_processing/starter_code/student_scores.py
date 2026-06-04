9392a6d585721d3c887afcd897c757e04995b626
Giratinate <caltho1@yahoo.com>
2026-06-04 14:20:11 -0400

    Added commentary.

--- labs/lab04_csv_file_processing/starter_code/student_scores.py
+++ labs/lab04_csv_file_processing/starter_code/student_scores.py
@@ -3,7 +3,7 @@
 # Complete this program so that it reads quiz score data from a CSV file,
 # cleans the data, computes student averages, and prints a report.
 
-#This
+#This function converts the strings of scores into integers
 def clean_score(score_text):
     """
     Convert a score string into an integer.
@@ -19,27 +19,32 @@ def clean_score(score_text):
     else:
         return None
 
-
+#Calculates the average score from a list
 def calculate_average(scores):
     """
     Calculate the average of a list of numeric scores.
 
     If the list is empty, return None.
     """
-    avg = 0
+    sum = 0
+    #counts valid scores
     valid = 0
     for score in scores:
+        #If score is empty, then the function moves on
         if score is None:
             pass
+        #If score is an int, then it is added to avg and the counter of valid scores goes up
         else:
-            avg += score
+            sum += score
             valid += 1
+    #If there are valid scores, then the sum is divided by the number of valid scores and returned
     if valid > 0:
-        return avg / valid
+        return sum / valid
+    #If there are no valid scores, then None is returned
     else:
         return None
 
-
+#This function reads the file and returns a list of dictionaries with the students' records in it
 def read_scores(filename):
     """
     Read student quiz scores from a CSV file.
@@ -66,19 +71,27 @@ def read_scores(filename):
         ...
     ]
     """
+    #List that will contain the dictionaries
     records = []
+    #Opens file
     with open(filename, "r") as f:
         # Reads a line of open file
         for line in f:
+            #Strips whitespace and splits the lines before turning them into a list
             list1 = line.strip().split(",")
+            #Skips first line since it will start with "name"
             if list1[0] == "name":
                 continue
+            #Makes a list of scores, using the clean_score function to turn them into ints and Nones
             score1 = (clean_score(list1[1]), clean_score(list1[2]), clean_score(list1[3]))
+            #Turns the list into a dictionary
             dict1 = {"name": list1[0], "scores": score1, "average": calculate_average(score1)}
+            #Adds list to scores
             records.append(dict1)
+    #returns the list
     return records
 
-
+#Calculates a letter grade based on the average
 def letter_grade(average):
     """
     Return a simple letter grade based on the average.
@@ -92,6 +105,7 @@ def letter_grade(average):
 
     If the average is None, return "N/A".
     """
+    #Goes through each minimum for a grade. If no minimum is met, then the letter returned is F
     if average >= 87:
         return "A"
     elif average >= 77:
@@ -100,22 +114,27 @@ def letter_grade(average):
         return "C"
     elif average >= 57:
         return "D"
+    #If the Average is None, then the letter is N/A
     elif average is None:
         return "N/A"
     else:
         return "F"
 
-
+#Prints out an individual report for each student
 def print_student_report(records):
     """
     Print one line of output for each student.
     """
+    #goes through each dictionary in the list
     for record in records:
+        #Name, average, and letter are assigned before printing the report. This was necessary for formatting reasons.
         name = record["name"]
         avg = record["average"]
         grade = letter_grade(avg)
+        #Prints out the report
         print(f"{name}: Average = {avg:.2f} Grade = {grade}")
 
+#Prints out a report for the whole class
 def print_class_summary(records):
     """
     Print a summary for the whole class.
@@ -126,31 +145,40 @@ def print_class_summary(records):
         highest average
         lowest average
     """
+    #Declaration of variables
+    #Students counts the number of students in the class
     students = 0
+    #Highest counts the highes grade in the class
     highest = 0
-    lowest = 200
-    avg = 0
+    #Lowest counts the lowest grade in the class. There cannot be a lower high than 0 and a higher low than 100.
+    lowest = 100
+    #sum track of the total score before being divided by the number of students to get the average
+    sum = 0
+    #Goes through each record, adds a student to the total, checks to see if the average is a new high or low, and adds score to total
     for record in records:
         students += 1
         if record["average"] > highest:
             highest = record["average"]
         if record["average"] < lowest:
             lowest = record["average"]
-        avg += record["average"]
+        sum += record["average"]
+    #Prints out the information
     print(f"Number of students: {students}")
-    print(f"Class average: {avg/students:.2f}")
+    print(f"Class average: {sum/students:.2f}")
     print(f"Highest average: {highest:.2f}")
     print(f"Lowest average: {lowest:.2f}")
     pass
 
-
+#Main function
 def main():
+    #Gets file
     filename = "../data/quiz_scores.csv"
-
+    #Gets a list of dictionaries using the read_scores function
     records = read_scores(filename)
-
+    #Calls student reports function
     print_student_report(records)
     print()
+    #Calls class summary function
     print_class_summary(records)
-
+#Calls Main function
 main()
