d5b5592b7c95d823206efdb69d0866f583e513ff
Giratinate <caltho1@yahoo.com>
2026-06-10 13:06:22 -0400

    Finished init function for time_duration.py

--- labs/lab07_operator_overloading_comparisons/starter_code/time_duration.py
+++ labs/lab07_operator_overloading_comparisons/starter_code/time_duration.py
@@ -8,26 +8,33 @@ class TimeDuration:
 
         You may assume the input values are non-negative integers.
         """
-        pass
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
 
     def total_seconds(self):
         """
         Return the total number of seconds represented by this duration.
         """
-        pass
+        total_seconds = 0
+        total_seconds += self.hours * 3600 + self.minutes * 60 + self.seconds
+        return total_seconds
 
     def __str__(self):
         """
         Return a readable string such as:
             1h 05m 09s
         """
-        pass
+        return f"{self.hours}h {self.minutes:02d}m {self.seconds:02d}s"
 
     def __eq__(self, other):
         """
         Return True if two TimeDuration objects represent the same duration.
         """
-        pass
+        if other == self.total_seconds():
+            return True
+        else:
+            return False
 
     def __lt__(self, other):
         """
