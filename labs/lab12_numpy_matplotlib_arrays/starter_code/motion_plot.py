c622257e83d5048e82431eccc6502277ee5721d3
Giratinate <caltho1@yahoo.com>
2026-06-22 13:32:17 -0400

    Made the position function.

--- labs/lab12_numpy_matplotlib_arrays/starter_code/motion_plot.py
+++ labs/lab12_numpy_matplotlib_arrays/starter_code/motion_plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def position(t, x0, v0, a):
-    pass
+    return x0 + v0 * t + 0.5 * a * np.square(t)
 
 def velocity(t, v0, a):
     pass
@@ -14,6 +14,7 @@ def main():
     #       for 0 < t < 10 seconds
     #
     # TODO: create time array using np.linspace
+    time = np.arange(0, 10, 1)
     # TODO: compute position and velocity arrays
     # TODO: make and save plots
     pass
