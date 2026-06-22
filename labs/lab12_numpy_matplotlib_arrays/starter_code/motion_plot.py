147d847d93113cfa2e8671891bc3302ddf55c3b4
Giratinate <caltho1@yahoo.com>
2026-06-22 13:37:06 -0400

    Made the velocity function and added the variables.

--- labs/lab12_numpy_matplotlib_arrays/starter_code/motion_plot.py
+++ labs/lab12_numpy_matplotlib_arrays/starter_code/motion_plot.py
@@ -5,7 +5,7 @@ def position(t, x0, v0, a):
     return x0 + v0 * t + 0.5 * a * np.square(t)
 
 def velocity(t, v0, a):
-    pass
+    return v0 + (a * t)
 
 def main():
     # Consider a projectile launched at a speed of 50 m/s and an angle of 45 degrees.
@@ -15,7 +15,15 @@ def main():
     #
     # TODO: create time array using np.linspace
     time = np.arange(0, 10, 1)
+    v0 = 50.0
+    angle = 30.0
+    ay = -9.805
+    ax = 0.0
+    x0 = 0.0
+    y0 = 0.0
     # TODO: compute position and velocity arrays
+    v0x = v0 * np.cos(angle * np.pi/180)
+    v0y = v0 * np.sin(angle * np.pi/180)
     # TODO: make and save plots
     pass
 
