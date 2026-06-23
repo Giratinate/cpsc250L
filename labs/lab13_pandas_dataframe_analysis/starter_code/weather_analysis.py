864ad1d0f825e5df2dd69d60ab1a390fb22da504
Giratinate <caltho1@yahoo.com>
2026-06-23 13:24:54 -0400

    Completed the add_celsius function.

--- labs/lab13_pandas_dataframe_analysis/starter_code/weather_analysis.py
+++ labs/lab13_pandas_dataframe_analysis/starter_code/weather_analysis.py
@@ -15,8 +15,16 @@ def print_summary(df):
 
 def add_celsius(df):
     # TODO: add columns for temperatures converted to Celsius
+    celsius_high = []
+    celsius_low = []
+    for value in df["high"]:
+        celsius_high.append((value-32)/1.8)
+    for value in df["low"]:
+        celsius_low.append((value-32)/1.8)
+    df["celsius high"] = celsius_high
+    df["celsius low"] = celsius_low
     # TODO: return modified dataframe
-    pass
+    return df
 
 
 def clean_temperature_range(df, t_low_cut, t_high_cut):
@@ -37,7 +45,7 @@ def main():
     dataframe = load_weather_data(filename)
 
     dataframe = add_celsius(dataframe)
-
+    print(dataframe)
     T_low_cut = 19.0
     T_high_cut = 31.0
     dataframe = clean_temperature_range(dataframe, T_low_cut, T_high_cut)
